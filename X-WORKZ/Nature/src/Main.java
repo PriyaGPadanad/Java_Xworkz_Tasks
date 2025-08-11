dd82fd7c0be5687471ae72ac9ca310a52fb20378
PriyaGPadanad <priyagpadanad@gmail.com>
2025-08-11 12:20:35 +0530

    Commits

--- X-WORKZ/Nature/src/Main.java
+++ X-WORKZ/Nature/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 
             System.out.println("i = " + i);
-            System.out.println("Running main");
+            System.out.println("Running main    ");
         }
     }
 }
